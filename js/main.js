eb68b0f17896afd39e154bd47941960493e807dc
Niakrist <ak_88dron@inbox.ru>
2024-01-19 14:58:10 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 import { films } from "./../api/films.js";
 
 
-
-
 const filmsEL = document.querySelector('.films');
 const dataFilms = films;
-
 
 
 dataFilms.forEach(film => {
@@ -25,12 +22,11 @@ console.log('window.innerHeight: ', window.innerHeight);
 console.log('window.innerWidth: ', window.innerWidth);
 console.log('document.body.offsetHeight: ', document.body.offsetHeight);
 
-
+let y = 0
+const filmsEl = document.querySelector('.films');
 if (window.innerWidth < window.innerHeight) {
-  const filmsEl = document.querySelector('.films');
+  console.log('mob');
   let posY = 0;
-  let y = 0
-
   filmsEl.addEventListener('touchmove', (e) => {
     const { clientY } = e.touches[0];
 
@@ -55,9 +51,8 @@ if (window.innerWidth < window.innerHeight) {
 
   })
 } else {
-  const filmsEl = document.querySelector('.films');
-  let y = 0
-  filmsEl.addEventListener('onscroll', (e) => {
+  console.log('dt');
+  filmsEl.addEventListener('wheel', (e) => {
     if (y >= 0) {
       y += e.deltaY;
       filmsEl.style.bottom = y + 'px'
@@ -71,3 +66,16 @@ if (window.innerWidth < window.innerHeight) {
     }
   })
 }
+
+
+const menuLinkEl = document.querySelectorAll('.nav-genre__link');
+
+
+menuLinkEl.forEach(menuItem => {
+  menuItem.addEventListener('click', (e) => {
+    e.preventDefault();
+    const navGenreEl = document.querySelector('.nav-genre');
+    const dataAtr = menuItem.dataset.toDisplace;
+    navGenreEl.style.left = dataAtr;
+  })
+})
